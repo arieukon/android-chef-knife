c9317494ae64325a5a0eeee1603c20ae8d1d1a95
Anshari Furqan <anshari@clappingape.com>
2018-11-28 20:47:31 +0700

    Toast

---
+++ chefKnifeLibrary/src/main/java/arieukon/com/chefknifelibrary/ToastMessage.java
@@ -0,0 +1,11 @@
+package arieukon.com.chefknifelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+
+    public static void show(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
