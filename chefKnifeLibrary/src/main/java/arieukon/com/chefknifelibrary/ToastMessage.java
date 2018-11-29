be4377b7ed68cd4bdc147d11743d59c981722e80
Anshari Furqan <anshari@clappingape.com>
2018-11-29 14:37:19 +0700

    Add Short Toast

--- chefKnifeLibrary/src/main/java/arieukon/com/chefknifelibrary/ToastMessage.java
+++ chefKnifeLibrary/src/main/java/arieukon/com/chefknifelibrary/ToastMessage.java
@@ -8,4 +8,8 @@ public class ToastMessage {
     public static void show(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
+
+    public static void showShort(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 }
